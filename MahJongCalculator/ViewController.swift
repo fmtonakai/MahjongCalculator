4855c4fdad9c6acfca760152fb573880aa2b4c13
fmtonakai <@fm.tonakai@gmail.com>
2020-02-13 00:10:19 +0900

    Initial Commit

---
+++ MahJongCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MahJongCalculator
+//
+//  Created by Fuke Masaki on 2020/02/13.
+//  Copyright © 2020 Fuke Masaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
